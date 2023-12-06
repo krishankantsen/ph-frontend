742717b76d44fa6bf1307ab0423bd8e0ad7d6c27
Krishan Kant Sen <skrishan111213@gmail.com>
2023-12-06 08:14:10 +0530

    new

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,126 @@
+"use client";
+import {
+  Button,
+  Container,
+  CssBaseline,
+  TextField,
+  Typography,
+  Box,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+
+const theme = createTheme();
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:5000/signin/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // sending data to backend
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (data.error) {
+        alert(data.error);
+
+        return;
+      } else {
+        localStorage.setItem("token", data.token);
+        alert(data.success);
+        router.push("/");
+        router.refresh();
+      }
+    } catch (error) {
+      console.error("Error during form submission:", error);
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs" >
+
+
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 16,
+            mb:1,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            boxShadow: 3,
+            p: 5,
+            borderRadius: "16px",
+            backgroundColor: "white",
+          }}
+        >
+          <Typography component="h1" variant="h5" sx={{ fontWeight: "bold" }}>
+            SignIn to you Account
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+            textAlign="center"
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
+              Sign In
+            </Button>
+          </Box>
+          <Typography>
+            Dont Have Account ?{" "}
+            <span
+              onClick={() => {
+                router.push("/signup");
+              }}
+              style={{
+                cursor: "pointer",
+                color: "#1976d2",
+                fontWeight: "bold",
+              }}
+            >
+              SignUp
+            </span>
+          </Typography>
+        </Box>
+        
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default SignIn;
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import { Box } from "@mui/material";
+
+export default function myHome() {
+  return <Box  sx={{mt:10}}>Home</Box>
+}
